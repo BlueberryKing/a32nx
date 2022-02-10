5b700dcac3e57a68161ebfd9b0c6e065677a8940
BBK <22713769+BlueberryKing@users.noreply.github.com>
2022-02-11 00:11:36 +0100

    Include path angle constraints on metadata

--- src/fmgc/src/guidance/lnav/legs/index.ts
+++ src/fmgc/src/guidance/lnav/legs/index.ts
@@ -31,6 +31,8 @@ export interface SpeedConstraint {
     speed: Knots,
 }
 
+export type PathAngleConstraint = Degrees;
+
 export abstract class FXLeg extends Leg {
     from: WayPoint;
 }
@@ -72,6 +74,11 @@ export function getSpeedConstraintFromWaypoint(wp: WayPoint): SpeedConstraint | 
     return undefined;
 }
 
+export function getPathAngleConstraintFromWaypoint(wp: WayPoint): PathAngleConstraint | undefined {
+    // Check for null and undefined, we do this because 0 is falsy
+    return wp.verticalAngle;
+}
+
 export function waypointToLocation(wp: WayPoint): LatLongData {
     const loc: LatLongData = {
         lat: wp.infos.coordinates.lat,
@@ -105,6 +112,11 @@ export interface LegMetadata {
     speedConstraint?: SpeedConstraint,
 
     /**
+     * Path angle constraint applicable to this leg
+     */
+    pathAngleConstraint?: PathAngleConstraint,
+
+    /**
      * UTC seconds required time of arrival applicable to the leg
      */
     rtaUtcSeconds?: Seconds,
@@ -126,11 +138,13 @@ export interface LegMetadata {
 export function legMetadataFromMsfsWaypoint(waypoint: WayPoint): LegMetadata {
     const altitudeConstraint = getAltitudeConstraintFromWaypoint(waypoint);
     const speedConstraint = getSpeedConstraintFromWaypoint(waypoint);
+    const pathAngleConstraint = getPathAngleConstraintFromWaypoint(waypoint);
 
     return {
         turnDirection: waypoint.turnDirection,
         altitudeConstraint,
         speedConstraint,
+        pathAngleConstraint,
         isOverfly: waypoint.additionalData.overfly,
     };
 }
