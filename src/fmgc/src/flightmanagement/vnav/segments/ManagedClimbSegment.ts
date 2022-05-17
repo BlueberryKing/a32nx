7b5dffaf03866dbe82be8a25b0b39f8dd0f96dad
BBK <22713769+BlueberryKing@users.noreply.github.com>
2022-05-17 21:34:53 +0200

    Fix missing acceleration segment after speed limit

--- src/fmgc/src/flightmanagement/vnav/segments/ManagedClimbSegment.ts
+++ src/fmgc/src/flightmanagement/vnav/segments/ManagedClimbSegment.ts
@@ -3,17 +3,19 @@ import { ClimbThrustSetting } from '@fmgc/flightmanagement/vnav/integrators';
 import { ClimbToAltConstraintSegment } from '@fmgc/flightmanagement/vnav/segments/ClimbToAltConstraintSegment';
 import { PureAccelerationSegment } from '@fmgc/flightmanagement/vnav/segments/PureAccelerationSegment';
 import { PureClimbToAltitudeSegment } from '@fmgc/flightmanagement/vnav/segments/PureClimbToAltitudeSegment';
-import { NodeContext } from '@fmgc/flightmanagement/vnav/segments/index';
+import { AircraftState, NodeContext, ProfileBuilder } from '@fmgc/flightmanagement/vnav/segments/index';
 import { ProfileSegment } from '@fmgc/flightmanagement/vnav/segments/ProfileSegment';
 
 export class ManagedClimbSegment extends ProfileSegment {
-    constructor(context: NodeContext, maxSpeed: Knots, toAltitude: Feet, constraints: ConstraintReader) {
+    private climbThrust: ClimbThrustSetting;
+
+    constructor(private context: NodeContext, private maxSpeed: Knots, private toAltitude: Feet, private constraints: ConstraintReader) {
         super();
 
-        const climbThrust = new ClimbThrustSetting(context.atmosphericConditions);
+        this.climbThrust = new ClimbThrustSetting(context.atmosphericConditions);
 
         this.children = [
-            new PureClimbToAltitudeSegment(context, climbThrust, toAltitude),
+            new PureClimbToAltitudeSegment(context, this.climbThrust, toAltitude),
         ];
 
         const allConstraints = [...constraints.climbAlitudeConstraints, ...constraints.climbSpeedConstraints];
@@ -23,22 +25,40 @@ export class ManagedClimbSegment extends ProfileSegment {
         let currentMaxAltitude = toAltitude;
         for (const constraint of allConstraints) {
             if ('maxSpeed' in constraint) {
-                this.children.push(new PureAccelerationSegment(context, climbThrust, currentMaxSpeed, currentMaxAltitude));
+                // Is speed constraint
+                this.children.push(new PureAccelerationSegment(context, this.climbThrust, currentMaxSpeed, currentMaxAltitude));
                 currentMaxSpeed = Math.min(currentMaxSpeed, constraint.maxSpeed);
-            } else {
-                this.children.push(new ClimbToAltConstraintSegment(context, climbThrust, constraint));
+            } else if (constraint.maxAltitude <= toAltitude) {
+                this.children.push(new ClimbToAltConstraintSegment(context, this.climbThrust, constraint));
                 currentMaxAltitude = Math.min(currentMaxAltitude, constraint.maxAltitude);
             }
         }
 
-        this.children.push(
-            new PureAccelerationSegment(context, climbThrust, currentMaxSpeed, currentMaxAltitude),
-        );
-
         this.children = this.children.reverse();
     }
 
+    compute(state: AircraftState, _builder: ProfileBuilder): void {
+        const maxSpeed = this.computeInitialMaxSpeed(state);
+        const maxAltitude = this.computeInitialMaxAltitude(state);
+
+        this.children.unshift(
+            new PureAccelerationSegment(this.context, this.climbThrust, maxSpeed, maxAltitude),
+        );
+    }
+
+    private computeInitialMaxSpeed(state: AircraftState): Knots {
+        return this.constraints.climbSpeedConstraints.reduce(
+            (previous, current) => (state.distanceFromStart <= current.distanceFromStart ? Math.min(previous, current.maxSpeed) : previous), this.maxSpeed,
+        );
+    }
+
+    private computeInitialMaxAltitude(state: AircraftState): Knots {
+        return this.constraints.climbAlitudeConstraints.reduce(
+            (previous, current) => (state.distanceFromStart <= current.distanceFromStart ? Math.min(previous, current.maxAltitude) : previous), this.toAltitude,
+        );
+    }
+
     get repr() {
-        return 'ManagedClimbNode';
+        return 'ManagedClimbSegment';
     }
 }
