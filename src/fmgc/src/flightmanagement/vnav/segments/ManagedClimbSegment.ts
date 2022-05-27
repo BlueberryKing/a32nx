e2ac3f42272656c990517f73aed3b9c7219d5671
BBK <22713769+BlueberryKing@users.noreply.github.com>
2022-05-27 22:15:42 +0200

    Fix climb profile logic issue

--- src/fmgc/src/flightmanagement/vnav/segments/ManagedClimbSegment.ts
+++ src/fmgc/src/flightmanagement/vnav/segments/ManagedClimbSegment.ts
@@ -6,6 +6,7 @@ import { PureClimbToAltitudeSegment } from '@fmgc/flightmanagement/vnav/segments
 import { AircraftState, NodeContext, ProfileBuilder } from '@fmgc/flightmanagement/vnav/segments/index';
 import { ProfileSegment } from '@fmgc/flightmanagement/vnav/segments/ProfileSegment';
 import { PureLevelAccelerationSegment } from '@fmgc/flightmanagement/vnav/segments/PureLevelAccelerationSegment';
+import { PureLevelSegment } from '@fmgc/flightmanagement/vnav/segments/PureLevelSegment';
 
 export class ManagedClimbSegment extends ProfileSegment {
     private climbThrust: ClimbThrustSetting;
@@ -26,13 +27,28 @@ export class ManagedClimbSegment extends ProfileSegment {
         let currentMaxAltitude = toAltitude;
         for (const constraint of allConstraints) {
             // Is speed constraint
+            // We only consider speed constraints if they are more restrictive than a later speed constraint
             if ('maxSpeed' in constraint) {
-                // Accelerate in level flight if an upcoming constraint requires it.
-                this.children.push(new PureLevelAccelerationSegment(context, this.climbThrust, currentMaxSpeed, currentMaxSpeed));
-                // Accelerate
-                this.children.push(new PureAccelerationSegment(context, this.climbThrust, currentMaxSpeed, currentMaxAltitude));
-                // Make sure we actually fly to the constraint
-                this.children.push(new ClimbToAltConstraintSegment(context, this.climbThrust, currentMaxAltitude, constraint.distanceFromStart));
+                // If this constraint is not more restrictive than a later one, we can ignore it.
+                if (constraint.maxSpeed >= currentMaxSpeed) {
+                    continue;
+                }
+
+                // We only need to do this if we're below `toAltitude` because if we're above it, we can let the next `ManagedClimbSegment` take care of them
+                if (currentMaxAltitude < toAltitude) {
+                    // The following two segments make sure we get to the next constraint after dealing with `constraint`.
+                    // Accelerate in level flight if an upcoming constraint requires it.
+                    this.children.push(new PureLevelAccelerationSegment(context, this.climbThrust, currentMaxSpeed, Infinity));
+                    // Accelerate after `constraint`
+                    this.children.push(new PureAccelerationSegment(context, this.climbThrust, currentMaxSpeed, currentMaxAltitude));
+
+                    // Possibly fly level before reaching `constraint` if there is a constraing altitude constraint
+                    this.children.push(new PureLevelSegment(context, constraint.distanceFromStart));
+                }
+
+                // Fly to `constraint` first.
+                this.children.push(new PureClimbToAltitudeSegment(context, this.climbThrust, currentMaxAltitude, false, constraint.distanceFromStart));
+
                 currentMaxSpeed = Math.min(currentMaxSpeed, constraint.maxSpeed);
             } else if (constraint.maxAltitude <= toAltitude) {
                 this.children.push(new ClimbToAltConstraintSegment(context, this.climbThrust, constraint.maxAltitude, constraint.distanceFromStart));
