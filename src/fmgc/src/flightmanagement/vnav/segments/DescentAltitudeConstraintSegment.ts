5ee9bbec2476a03dad11594d59d3f5a13cebca20
BBK <22713769+BlueberryKing@users.noreply.github.com>
2022-06-04 01:47:46 +0200

    Improve Geometric path construction

--- src/fmgc/src/flightmanagement/vnav/segments/DescentAltitudeConstraintSegment.ts
+++ src/fmgc/src/flightmanagement/vnav/segments/DescentAltitudeConstraintSegment.ts
@@ -1,17 +1,14 @@
 import { PureConstantFlightPathAngleSegment } from '@fmgc/flightmanagement/vnav/segments/PureConstantFlightPathAngleSegment';
 import { PureGeometricDecelerationSegment } from '@fmgc/flightmanagement/vnav/segments/PureGeometricDecelerationSegment';
-import { ConstraintReader, DescentAltitudeConstraint } from '@fmgc/guidance/vnav/ConstraintReader';
+import { ConstraintReader } from '@fmgc/guidance/vnav/ConstraintReader';
 import { AircraftState, NodeContext, ProfileBuilder } from '@fmgc/flightmanagement/vnav/segments';
 import { ProfileSegment } from '@fmgc/flightmanagement/vnav/segments/ProfileSegment';
-import { MathUtils } from '@shared/MathUtils';
-import { AltitudeConstraint, AltitudeConstraintType } from '@fmgc/guidance/lnav/legs';
-import { VnavConfig } from '@fmgc/guidance/vnav/VnavConfig';
 
 export class DescentAltitudeConstraintSegment extends ProfileSegment {
     constructor(
         private context: NodeContext,
         private constraints: ConstraintReader,
-        private constraint: DescentAltitudeConstraint,
+        private toDistance: NauticalMiles,
         private flightPathAngle: Degrees,
         private maxSpeed: Knots,
         private maxAltitude: Feet,
@@ -26,20 +23,15 @@ export class DescentAltitudeConstraintSegment extends ProfileSegment {
             return;
         }
 
-        const altAtConstraint = state.altitude + 6076.12 * (this.constraint.distanceFromStart - state.distanceFromStart) * Math.tan(this.flightPathAngle * MathUtils.DEGREES_TO_RADIANS);
-        if (VnavConfig.DEBUG_PROFILE && !this.isAltitudeConstraintMet(altAtConstraint, this.constraint.constraint)) {
-            console.warn(`[FMS/VNAV] Expecting to miss constraint. Predicting altitude ${altAtConstraint.toFixed(0)}, but should be at ${JSON.stringify(this.constraint.constraint)}`);
-        }
-
         let maxSpeed = this.computeMaxSpeedOnSegment();
 
         this.children = [
-            new PureGeometricDecelerationSegment(this.context, this.flightPathAngle, maxSpeed, this.constraint.distanceFromStart, this.maxAltitude),
-            new PureConstantFlightPathAngleSegment(this.context, this.flightPathAngle, this.constraint.distanceFromStart, this.maxAltitude, this.useMachVsCas),
+            new PureGeometricDecelerationSegment(this.context, this.flightPathAngle, maxSpeed, this.toDistance, this.maxAltitude),
+            new PureConstantFlightPathAngleSegment(this.context, this.flightPathAngle, this.toDistance, this.maxAltitude, this.useMachVsCas),
         ];
 
         for (const speedConstraint of this.constraints.descentSpeedConstraints) {
-            if (speedConstraint.distanceFromStart <= this.constraint.distanceFromStart) {
+            if (speedConstraint.distanceFromStart <= this.toDistance) {
                 continue;
             } else if (speedConstraint.distanceFromStart > state.distanceFromStart) {
                 break;
@@ -57,26 +49,6 @@ export class DescentAltitudeConstraintSegment extends ProfileSegment {
         }
     }
 
-    private isAltitudeConstraintMet(altitude: Feet, constraint?: AltitudeConstraint): boolean {
-        if (!constraint) {
-            return true;
-        }
-
-        switch (constraint.type) {
-        case AltitudeConstraintType.at:
-            return Math.abs(altitude - constraint.altitude1) < 250;
-        case AltitudeConstraintType.atOrAbove:
-            return (altitude - constraint.altitude1) > -250;
-        case AltitudeConstraintType.atOrBelow:
-            return (altitude - constraint.altitude1) < 250;
-        case AltitudeConstraintType.range:
-            return (altitude - constraint.altitude2) > -250 && (altitude - constraint.altitude1) < 250;
-        default:
-            console.error('Invalid altitude constraint type');
-            return null;
-        }
-    }
-
     get repr(): string {
         return `DescentAltitudeConstraintSegment - Descend at ${this.flightPathAngle.toFixed(2)}°`;
     }
@@ -84,7 +56,7 @@ export class DescentAltitudeConstraintSegment extends ProfileSegment {
     private computeMaxSpeedOnSegment(): Knots {
         // Use constraints before this segment to find the actual maxSpeed in this segment.
         return this.constraints.descentSpeedConstraints.reduce(
-            (previous, current) => (current.distanceFromStart <= this.constraint.distanceFromStart ? Math.min(previous, current.maxSpeed) : previous), this.maxSpeed,
+            (previous, current) => (current.distanceFromStart <= this.toDistance ? Math.min(previous, current.maxSpeed) : previous), this.maxSpeed,
         );
     }
 }
--- src/fmgc/src/flightmanagement/vnav/segments/GeometricPathSegment.ts
+++ src/fmgc/src/flightmanagement/vnav/segments/GeometricPathSegment.ts
@@ -3,6 +3,7 @@ import { MathUtils } from '@shared/MathUtils';
 import { DescentAltitudeConstraintSegment } from '@fmgc/flightmanagement/vnav/segments/DescentAltitudeConstraintSegment';
 import { AircraftState, NodeContext, ProfileBuilder } from '@fmgc/flightmanagement/vnav/segments';
 import { ProfileSegment } from '@fmgc/flightmanagement/vnav/segments/ProfileSegment';
+import { AltitudeConstraintType } from '@fmgc/guidance/lnav/legs';
 
 export class GeometricPathSegment extends ProfileSegment {
     /**
@@ -32,25 +33,86 @@ export class GeometricPathSegment extends ProfileSegment {
             return;
         }
 
-        const preferredFlightPathAngle = this.calculatePreferredFlightPathAngle(state);
+        const geometricPathEndPoint = { distanceFromStart: this.lastConstraint.distanceFromStart, altitude: this.lastConstraint.constraint.altitude1 };
+        const plannedSegments = [];
+        this.planDescentSegments(state, geometricPathEndPoint, plannedSegments);
 
-        for (const constraint of this.constraints.descentAltitudeConstraints) {
-            if (constraint.distanceFromStart > state.distanceFromStart || constraint.distanceFromStart < this.lastConstraint.distanceFromStart) {
-                continue;
-            }
+        for (const plannedSegment of plannedSegments) {
+            const fpa = MathUtils.RADIANS_TO_DEGREES * Math.atan(plannedSegment.gradient / 6076.12);
 
             this.children.unshift(
-                new DescentAltitudeConstraintSegment(this.context, this.constraints, constraint, preferredFlightPathAngle, this.maxSpeed, this.toAltitude, this.useMachVsCas),
+                new DescentAltitudeConstraintSegment(this.context, this.constraints, plannedSegment.end.distanceFromStart, fpa, this.maxSpeed, this.toAltitude, this.useMachVsCas),
             );
         }
     }
 
-    calculatePreferredFlightPathAngle(state: AircraftState) {
-        return MathUtils.RADIANS_TO_DEGREES
-            * Math.atan2(state.altitude - this.lastConstraint.constraint.altitude1, 6076.12 * (state.distanceFromStart - this.lastConstraint.distanceFromStart));
+    private planDescentSegments(start: GeometricPathPoint, end: GeometricPathPoint, segments: PlannedGeometricSegment[]): PlannedGeometricSegment[] {
+        // A "gradient" is just a quantity of units Feet / NauticalMiles
+        const gradient = this.calculateGradient(start, end);
+        const constraints = this.constraints.descentAltitudeConstraints;
+
+        for (let i = 0; i < constraints.length; i++) {
+            const constraint = constraints[i];
+
+            if (constraint.distanceFromStart > start.distanceFromStart || constraint.distanceFromStart <= end.distanceFromStart) {
+                continue;
+            }
+
+            const altAtConstraint = start.altitude + gradient * (constraint.distanceFromStart - start.distanceFromStart);
+            const constraintType = constraint.constraint.type;
+
+            if (constraintType === AltitudeConstraintType.at && Math.abs(altAtConstraint - constraint.constraint.altitude1) > 250) {
+                const center = { distanceFromStart: constraint.distanceFromStart, altitude: constraint.constraint.altitude1 };
+
+                this.planDescentSegments(start, center, segments);
+                this.planDescentSegments(center, end, segments);
+
+                return;
+            } if (constraintType === AltitudeConstraintType.atOrAbove && constraint.constraint.altitude1 - altAtConstraint > 250) {
+                const center = { distanceFromStart: constraint.distanceFromStart, altitude: constraint.constraint.altitude1 };
+
+                this.planDescentSegments(start, center, segments);
+                this.planDescentSegments(center, end, segments);
+
+                return;
+            } if (constraintType === AltitudeConstraintType.atOrBelow && altAtConstraint - constraint.constraint.altitude1 > 250) {
+                const center = { distanceFromStart: constraint.distanceFromStart, altitude: constraint.constraint.altitude1 };
+
+                this.planDescentSegments(start, center, segments);
+                this.planDescentSegments(center, end, segments);
+
+                return;
+            } if (constraintType === AltitudeConstraintType.range && constraint.constraint.altitude2 - altAtConstraint > 250 || altAtConstraint - constraint.constraint.altitude1 > 250) {
+                const altitude = Math.min(constraint.constraint.altitude2, Math.max(altAtConstraint, constraint.constraint.altitude1));
+                const center = { distanceFromStart: constraint.distanceFromStart, altitude };
+
+                this.planDescentSegments(start, center, segments);
+                this.planDescentSegments(center, end, segments);
+
+                return;
+            }
+        }
+
+        segments.push({ end, gradient });
+    }
+
+    calculateGradient(start: GeometricPathPoint, end: GeometricPathPoint) {
+        return Math.abs(start.distanceFromStart - end.distanceFromStart) < 1e-12
+            ? 0
+            : (start.altitude - end.altitude) / (start.distanceFromStart - end.distanceFromStart);
     }
 
     get repr(): string {
         return 'GeometricPathSegment';
     }
+}
+
+type GeometricPathPoint = {
+    distanceFromStart: NauticalMiles,
+    altitude: Feet
+}
+
+type PlannedGeometricSegment = {
+    gradient: number,
+    end: GeometricPathPoint,
 }
