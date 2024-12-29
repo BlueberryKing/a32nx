7ef35698d6480930d3158812a10169f59de0c881
BBK <22713769+BlueberryKing@users.noreply.github.com>
2024-12-29 22:12:42 +0100

    Implement NO NAV INTERCEPT message

---
+++ fbw-a32nx/src/systems/fmgc/src/components/fms-messages/NoNavIntercept.ts
@@ -0,0 +1,32 @@
+// Copyright (c) 2021-2023 FlyByWire Simulations
+//
+// SPDX-License-Identifier: GPL-3.0
+
+import { FMMessage, FMMessageTypes } from '@flybywiresim/fbw-sdk';
+
+import { GuidanceController } from '@fmgc/guidance/GuidanceController';
+import { FMMessageSelector, FMMessageUpdate } from './FmsMessages';
+
+export class NoNavIntercept implements FMMessageSelector {
+  message: FMMessage = FMMessageTypes.NoNavIntercept;
+
+  private guidanceController: GuidanceController;
+
+  private lastState = false;
+
+  init(baseInstrument: BaseInstrument): void {
+    this.guidanceController = baseInstrument.guidanceController;
+  }
+
+  process(_: number): FMMessageUpdate {
+    const newState = this.guidanceController.shouldShowNoNavInterceptMessage();
+
+    if (newState !== this.lastState) {
+      this.lastState = newState;
+
+      return newState ? FMMessageUpdate.SEND : FMMessageUpdate.RECALL;
+    }
+
+    return FMMessageUpdate.NO_ACTION;
+  }
+}
--- fbw-a32nx/src/systems/fmgc/src/guidance/GuidanceController.ts
+++ fbw-a32nx/src/systems/fmgc/src/guidance/GuidanceController.ts
@@ -587,4 +587,8 @@ export class GuidanceController {
   getPreNavModeEngagementPathIntercept(): Readonly<NavModeIntercept> | null {
     return this.preNavModeEngagementPath.getIntercept();
   }
+
+  shouldShowNoNavInterceptMessage(): boolean {
+    return this.preNavModeEngagementPath.shouldShowNoNavInterceptMessage();
+  }
 }
