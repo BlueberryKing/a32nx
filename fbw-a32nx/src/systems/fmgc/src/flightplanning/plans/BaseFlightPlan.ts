5ff6e465918e1932a40d1c87524bc8ead464dcaa
BBK <22713769+BlueberryKing@users.noreply.github.com>
2025-06-04 19:43:17 +0200

    Handle wind interpolation across a discontinuity

--- fbw-a32nx/src/systems/fmgc/src/flightplanning/plans/BaseFlightPlan.ts
+++ fbw-a32nx/src/systems/fmgc/src/flightplanning/plans/BaseFlightPlan.ts
@@ -2715,7 +2715,7 @@ export abstract class BaseFlightPlan<P extends FlightPlanPerformanceData = Fligh
     this.incrementVersion();
   }
 
-  protected hasLegAt(index: number): boolean {
+  hasLegAt(index: number): boolean {
     return isLeg(this.maybeElementAt(index));
   }
 
